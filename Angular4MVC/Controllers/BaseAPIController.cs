ef521a52ef6b325da2612af409b72fdf96bcec6c
Udara Fernando <ucf@tiqri.com>
2018-08-06 11:08:31 +0530

    Initial push

---
+++ Angular4MVC/Controllers/BaseAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Angular4MVC.DBContext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Angular4MVC.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ Angular4MVC/app/Components/home.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    template: `<img src="../../images/users.jpg" style="text-align:center"/>`
+})
+
+export class HomeComponent {
+}
